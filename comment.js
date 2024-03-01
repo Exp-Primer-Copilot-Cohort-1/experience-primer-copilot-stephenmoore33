d47cc246cd1ed79f690af74450057be5d0341dbc
Stephen Moore <StMoore@wcskids.net>
2024-03-01 14:49:59 -0500

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,12 +1,1 @@
-function skillsMember() {
-	return {
-		restrict: 'E',
-		templateUrl: 'modules/skills/views/member.html',
-		controller: 'SkillsMemberController',
-		controllerAs: 'vm',
-		bindToController: true,
-		scope: {
-			member: '='
-			}
-		};
-	}
+// Create web server
